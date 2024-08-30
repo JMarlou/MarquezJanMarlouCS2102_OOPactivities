c7040bf3476bb0a6e192a3bf97db2ec830fdce74
JMarlou <165748727+JMarlou@users.noreply.github.com>
2024-08-30 06:56:16 +0000

    temp disappeared >:0000

--- Exercise Sets/Exercise Set 01/Tempconvert.java
+++ Exercise Sets/Exercise Set 01/Tempconvert.java
@@ -1,3 +1,17 @@
-public class Tempconvert {
-    
+import java.util.Scanner;
+
+public class Tempconvert
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Celsius calue: ");
+
+        double Celsius = scanner.nextDouble();
+        double Fahrenheit = (Celsius * 9)/5 + 32;
+        
+        System.out.println(Celsius + " Celsius is " + Fahrenheit + " Fahrenheit");
+
+        scanner.close();
+    }
 }
