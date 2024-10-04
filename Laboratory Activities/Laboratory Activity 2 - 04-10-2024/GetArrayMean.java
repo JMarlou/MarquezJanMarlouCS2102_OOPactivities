a4e951e77afe3e5b4ac1f1388b0737bde519edcc
JMarlou <165748727+JMarlou@users.noreply.github.com>
2024-10-04 08:10:48 +0000

    donee

--- Laboratory Activities/Laboratory Activity 2 - 04-10-2024/GetArrayMean.java
+++ Laboratory Activities/Laboratory Activity 2 - 04-10-2024/GetArrayMean.java
@@ -1,3 +1,30 @@
-public class GetArrayMean {
+import java.util.Scanner;
 
+public class GetArrayMean {
+    public static double ArrayMean(int[] arr){
+        if (arr.length == 0) return 0;
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of Elements: ");
+        int n = scanner.nextInt();
+        int[] integers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter an integer: ");
+            integers[i] = scanner.nextInt();
+        }
+
+        double mean = ArrayMean(integers);
+        System.out.printf("Mean of array is %.2f%n", mean);
+
+        scanner.close();
+};
 }
--- Laboratory Activities/Laboratory Activity 2 - 04-10-2024/MultiplicationTable.java
+++ Laboratory Activities/Laboratory Activity 2 - 04-10-2024/MultiplicationTable.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
 public class MultiplicationTable{
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
 
+        System.out.print("Enter the size of the multiplication table: ");
+        int max = scanner.nextInt();
 
-    
+        int[][] multiplicationTable = new int[max + 1][max + 1];
+
+        for(int i = 1; i <= max; i++){
+            for(int j = 1; j <= max; j++){
+                multiplicationTable[i][j] = i * j;
+        }
+    } 
+        System.out.println("Multiplication Table:");
+        for(int i = 1;  i <= max; i++){
+            for(int j = 1; j <= max; j++){
+                System.out.printf("%4d", multiplicationTable[i][j]);
+            }
+            System.out.println();
+        }
+    scanner.close();
+    }
 }
